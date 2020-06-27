a0d2ebe8e75e0e590f03df2b48d75cf43cdb05d8
Travis McGaha <tqmcgaha@attu1.cs.washington.edu>
2020-06-26 22:32:44 -0700

    Files added by CSE 333 staff

---
+++ hw1/HashTable.c
@@ -0,0 +1,288 @@
+/*
+ * Copyright ©2020 Travis McGaha.  All rights reserved.  Permission is
+ * hereby granted to students registered for University of Washington
+ * CSE 333 for use solely during Summer Quarter 2020 for purposes of
+ * the course.  No other use, copying, distribution, or modification
+ * is permitted without prior written consent. Copyrights for
+ * third-party components of this work must be honored.  Instructors
+ * interested in reusing these course materials should contact the
+ * author.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "CSE333.h"
+#include "HashTable.h"
+#include "HashTable_priv.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// Internal helper functions.
+//
+#define INVALID_IDX -1
+
+// Grows the hashtable (ie, increase the number of buckets) if its load
+// factor has become too high.
+static void MaybeResize(HashTable *ht);
+
+int HashKeyToBucketNum(HashTable *ht, HTKey_t key) {
+  return key % ht->num_buckets;
+}
+
+// Deallocation functions that do nothing.  Useful if we want to deallocate
+// the structure (eg, the linked list) without deallocating its elements or
+// if we know that the structure is empty.
+static void LLNoOpFree(LLPayload_t freeme) { }
+static void HTNoOpFree(HTValue_t freeme) { }
+
+
+///////////////////////////////////////////////////////////////////////////////
+// HashTable implementation.
+
+HTKey_t FNVHash64(unsigned char *buffer, int len) {
+  // This code is adapted from code by Landon Curt Noll
+  // and Bonelli Nicola:
+  //     http://code.google.com/p/nicola-bonelli-repo/
+  static const uint64_t FNV1_64_INIT = 0xcbf29ce484222325ULL;
+  static const uint64_t FNV_64_PRIME = 0x100000001b3ULL;
+  unsigned char *bp = (unsigned char *) buffer;
+  unsigned char *be = bp + len;
+  uint64_t hval = FNV1_64_INIT;
+
+  // FNV-1a hash each octet of the buffer.
+  while (bp < be) {
+    // XOR the bottom with the current octet.
+    hval ^= (uint64_t) * bp++;
+    // Multiply by the 64 bit FNV magic prime mod 2^64.
+    hval *= FNV_64_PRIME;
+  }
+  return hval;
+}
+
+HashTable* HashTable_Allocate(int num_buckets) {
+  HashTable *ht;
+  int i;
+
+  Verify333(num_buckets > 0);
+
+  // Allocate the hash table record.
+  ht = (HashTable *) malloc(sizeof(HashTable));
+
+  // Initialize the record.
+  ht->num_buckets = num_buckets;
+  ht->num_elements = 0;
+  ht->buckets = (LinkedList **) malloc(num_buckets * sizeof(LinkedList *));
+  for (i = 0; i < num_buckets; i++) {
+    ht->buckets[i] = LinkedList_Allocate();
+  }
+
+  return ht;
+}
+
+void HashTable_Free(HashTable *table,
+                    ValueFreeFnPtr value_free_function) {
+  int i;
+
+  Verify333(table != NULL);
+
+  // Free each bucket's chain.
+  for (i = 0; i < table->num_buckets; i++) {
+    LinkedList *bucket = table->buckets[i];
+    HTKeyValue_t *kv;
+
+    // Pop elements off the chain list one at a time.  We can't do a single
+    // call to LinkedList_Free since we need to use the passed-in
+    // value_free_function -- which takes a HTValue_t, not an LLPayload_t -- to
+    // free the caller's memory.
+    while (LinkedList_NumElements(bucket) > 0) {
+      Verify333(LinkedList_Pop(bucket, (LLPayload_t *)&kv));
+      value_free_function(kv->value);
+      free(kv);
+    }
+    // The chain is empty, so we can pass in the
+    // null free function to LinkedList_Free.
+    LinkedList_Free(bucket, LLNoOpFree);
+  }
+
+  // Free the bucket array within the table, then free the table record itself.
+  free(table->buckets);
+  free(table);
+}
+
+int HashTable_NumElements(HashTable *table) {
+  Verify333(table != NULL);
+  return table->num_elements;
+}
+
+bool HashTable_Insert(HashTable *table,
+                      HTKeyValue_t newkeyvalue,
+                      HTKeyValue_t *oldkeyvalue) {
+  int bucket;
+  LinkedList *chain;
+
+  Verify333(table != NULL);
+  MaybeResize(table);
+
+  // Calculate which bucket and chain we're inserting into.
+  bucket = HashKeyToBucketNum(table, newkeyvalue.key);
+  chain = table->buckets[bucket];
+
+  // STEP 1: finish the implementation of InsertHashTable.
+  // This is a fairly complex task, so you might decide you want
+  // to define/implement a helper function that helps you find
+  // and optionally remove a key within a chain, rather than putting
+  // all that logic inside here.  You might also find that your helper
+  // can be reused in steps 2 and 3.
+
+  return 0;  // you may need to change this return value
+}
+
+bool HashTable_Find(HashTable *table,
+                    HTKey_t key,
+                    HTKeyValue_t *keyvalue) {
+  Verify333(table != NULL);
+
+  // STEP 2: implement HashTable_Find.
+
+  return false;  // you may need to change this return value
+}
+
+bool HashTable_Remove(HashTable *table,
+                      HTKey_t key,
+                      HTKeyValue_t *keyvalue) {
+  Verify333(table != NULL);
+
+  // STEP 3: implement HashTable_Remove.
+
+  return 0;  // you may need to change this return value
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// HTIterator implementation.
+
+HTIterator* HTIterator_Allocate(HashTable *table) {
+  HTIterator *iter;
+  int         i;
+
+  Verify333(table != NULL);
+
+  iter = (HTIterator *) malloc(sizeof(HTIterator));
+
+  // If the hash table is empty, the iterator is immediately invalid,
+  // since it can't point to anything.
+  if (table->num_elements == 0) {
+    iter->ht = table;
+    iter->bucket_it = NULL;
+    iter->bucket_idx = INVALID_IDX;
+    return iter;
+  }
+
+  // Initialize the iterator.  There is at least one element in the
+  // table, so find the first element and point the iterator at it.
+  iter->ht = table;
+  for (i = 0; i < table->num_buckets; i++) {
+    if (LinkedList_NumElements(table->buckets[i]) > 0) {
+      iter->bucket_idx = i;
+      break;
+    }
+  }
+  Verify333(i < table->num_buckets);  // make sure we found it.
+  iter->bucket_it = LLIterator_Allocate(table->buckets[iter->bucket_idx]);
+  return iter;
+}
+
+void HTIterator_Free(HTIterator *iter) {
+  Verify333(iter != NULL);
+  if (iter->bucket_it != NULL) {
+    LLIterator_Free(iter->bucket_it);
+    iter->bucket_it = NULL;
+  }
+  free(iter);
+}
+
+bool HTIterator_IsValid(HTIterator *iter) {
+  Verify333(iter != NULL);
+
+  // STEP 4: implement HTIterator_IsValid.
+
+  return true;  // you may need to change this return value
+}
+
+bool HTIterator_Next(HTIterator *iter) {
+  Verify333(iter != NULL);
+
+  // STEP 5: implement HTIterator_Next.
+
+  return true;  // you may need to change this return value
+}
+
+bool HTIterator_Get(HTIterator *iter, HTKeyValue_t *keyvalue) {
+  Verify333(iter != NULL);
+
+  // STEP 6: implement HTIterator_Get.
+
+  return true;  // you may need to change this return value
+}
+
+bool HTIterator_Remove(HTIterator *iter, HTKeyValue_t *keyvalue) {
+  HTKeyValue_t kv;
+
+  Verify333(iter != NULL);
+
+  // Try to get what the iterator is pointing to.
+  if (!HTIterator_Get(iter, &kv)) {
+    return false;
+  }
+
+  // Advance the iterator.  Thanks to the above call to
+  // HTIterator_Get, we know that this iterator is valid (though it
+  // may not be valid after this call to HTIterator_Next).
+  HTIterator_Next(iter);
+
+  // Lastly, remove the element.  Again, we know this call will succeed
+  // due to the successful HTIterator_Get above.
+  Verify333(HashTable_Remove(iter->ht, kv.key, keyvalue));
+  Verify333(kv.key == keyvalue->key);
+  Verify333(kv.value == keyvalue->value);
+
+  return true;
+}
+
+static void MaybeResize(HashTable *ht) {
+  HashTable *newht;
+  HashTable tmp;
+  HTIterator *it;
+
+  // Resize if the load factor is > 3.
+  if (ht->num_elements < 3 * ht->num_buckets)
+    return;
+
+  // This is the resize case.  Allocate a new hashtable,
+  // iterate over the old hashtable, do the surgery on
+  // the old hashtable record and free up the new hashtable
+  // record.
+  newht = HashTable_Allocate(ht->num_buckets * 9);
+
+  // Loop through the old ht copying its elements over into the new one.
+  for (it = HTIterator_Allocate(ht);
+       HTIterator_IsValid(it);
+       HTIterator_Next(it)) {
+    HTKeyValue_t item, unused;
+
+    Verify333(HTIterator_Get(it, &item));
+    HashTable_Insert(newht, item, &unused);
+  }
+
+  // Swap the new table onto the old, then free the old table (tricky!).  We
+  // use the "no-op free" because we don't actually want to free the elements;
+  // they're owned by the new table.
+  tmp = *ht;
+  *ht = *newht;
+  *newht = tmp;
+
+  // Done!  Clean up our iterator and temporary table.
+  HTIterator_Free(it);
+  HashTable_Free(newht, &HTNoOpFree);
+}
---
+++ hw1/LinkedList.c
@@ -0,0 +1,216 @@
+/*
+ * Copyright ©2020 Travis McGaha.  All rights reserved.  Permission is
+ * hereby granted to students registered for University of Washington
+ * CSE 333 for use solely during Summer Quarter 2020 for purposes of
+ * the course.  No other use, copying, distribution, or modification
+ * is permitted without prior written consent. Copyrights for
+ * third-party components of this work must be honored.  Instructors
+ * interested in reusing these course materials should contact the
+ * author.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "CSE333.h"
+#include "LinkedList.h"
+#include "LinkedList_priv.h"
+
+
+///////////////////////////////////////////////////////////////////////////////
+// LinkedList implementation.
+
+LinkedList* LinkedList_Allocate(void) {
+  // Allocate the linked list record.
+  LinkedList *ll = (LinkedList *) malloc(sizeof(LinkedList));
+  Verify333(ll != NULL);
+
+  // STEP 1: initialize the newly allocated record structure.
+
+  // Return our newly minted linked list.
+  return ll;
+}
+
+void LinkedList_Free(LinkedList *list,
+                     LLPayloadFreeFnPtr payload_free_function) {
+  Verify333(list != NULL);
+  Verify333(payload_free_function != NULL);
+
+  // STEP 2: sweep through the list and free all of the nodes' payloads as
+  // well as the nodes themselves.
+  while (list->head != NULL) {
+  }
+
+  free(list);
+}
+
+int LinkedList_NumElements(LinkedList *list) {
+  Verify333(list != NULL);
+  return list->num_elements;
+}
+
+void LinkedList_Push(LinkedList *list, LLPayload_t payload) {
+  Verify333(list != NULL);
+
+  // Allocate space for the new node.
+  LinkedListNode *ln = (LinkedListNode *) malloc(sizeof(LinkedListNode));
+  Verify333(ln != NULL);
+
+  // Set the payload
+  ln->payload = payload;
+
+  if (list->num_elements == 0) {
+    // Degenerate case; list is currently empty
+    Verify333(list->head == NULL);
+    Verify333(list->tail == NULL);
+    ln->next = ln->prev = NULL;
+    list->head = list->tail = ln;
+    list->num_elements = 1;
+  } else {
+    // STEP 3: typical case; list has >=1 elements
+  }
+}
+
+bool LinkedList_Pop(LinkedList *list, LLPayload_t *payload_ptr) {
+  Verify333(payload_ptr != NULL);
+  Verify333(list != NULL);
+
+  // STEP 4: implement LinkedList_Pop.  Make sure you test for
+  // and empty list and fail.  If the list is non-empty, there
+  // are two cases to consider: (a) a list with a single element in it
+  // and (b) the general case of a list with >=2 elements in it.
+  // Be sure to call free() to deallocate the memory that was
+  // previously allocated by LinkedList_Push().
+
+  return true;  // you may need to change this return value
+}
+
+void LinkedList_Append(LinkedList *list, LLPayload_t payload) {
+  Verify333(list != NULL);
+
+  // STEP 5: implement LinkedList_Append.  It's kind of like
+  // LinkedList_Push, but obviously you need to add to the end
+  // instead of the beginning.
+}
+
+void LinkedList_Sort(LinkedList *list, bool ascending,
+                     LLPayloadComparatorFnPtr comparator_function) {
+  Verify333(list != NULL);
+  if (list->num_elements < 2) {
+    // No sorting needed.
+    return;
+  }
+
+  // We'll implement bubblesort! Nnice and easy, and nice and slow :)
+  int swapped;
+  do {
+    LinkedListNode *curnode;
+
+    swapped = 0;
+    curnode = list->head;
+    while (curnode->next != NULL) {
+      int compare_result = comparator_function(curnode->payload,
+                                               curnode->next->payload);
+      if (ascending) {
+        compare_result *= -1;
+      }
+      if (compare_result < 0) {
+        // Bubble-swap the payloads.
+        LLPayload_t tmp;
+        tmp = curnode->payload;
+        curnode->payload = curnode->next->payload;
+        curnode->next->payload = tmp;
+        swapped = 1;
+      }
+      curnode = curnode->next;
+    }
+  } while (swapped);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// LLIterator implementation.
+
+LLIterator* LLIterator_Allocate(LinkedList *list) {
+  Verify333(list != NULL);
+
+  // OK, let's manufacture an iterator.
+  LLIterator *li = (LLIterator *) malloc(sizeof(LLIterator));
+  Verify333(li != NULL);
+
+  // Set up the iterator.
+  li->list = list;
+  li->node = list->head;
+
+  return li;
+}
+
+void LLIterator_Free(LLIterator *iter) {
+  Verify333(iter != NULL);
+  free(iter);
+}
+
+bool LLIterator_IsValid(LLIterator *iter) {
+  Verify333(iter != NULL);
+  Verify333(iter->list != NULL);
+
+  return (iter->node != NULL);
+}
+
+bool LLIterator_Next(LLIterator *iter) {
+  Verify333(iter != NULL);
+  Verify333(iter->list != NULL);
+  Verify333(iter->node != NULL);
+
+  // STEP 6: advance to the node beyond the iterator and return true if
+  // it was valid.
+
+  return true;  // you may need to change this return value
+}
+
+void LLIterator_Get(LLIterator *iter, LLPayload_t *payload) {
+  Verify333(iter != NULL);
+  Verify333(iter->list != NULL);
+  Verify333(iter->node != NULL);
+
+  *payload = iter->node->payload;
+}
+
+bool LLIterator_Remove(LLIterator *iter,
+                       LLPayloadFreeFnPtr payload_free_function) {
+  Verify333(iter != NULL);
+  Verify333(iter->list != NULL);
+  Verify333(iter->node != NULL);
+
+  // STEP 7: implement LLIterator_Remove.  This is the most
+  // complex function you'll build.  There are several cases
+  // to consider:
+  // - degenerate case: the list becomes empty after deleting.
+  // - degenerate case: iter points at head
+  // - degenerate case: iter points at tail
+  // - fully general case: iter points in the middle of a list,
+  //                       and you have to "splice".
+  //
+  // Be sure to call the payload_free_function to free the payload
+  // the iterator is pointing to, and also free any LinkedList
+  // data structure element as appropriate.
+
+  return true;  // you may need to change this return value
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Helper functions
+
+bool LinkedList_Slice(LinkedList *list, LLPayload_t *payload_ptr) {
+  Verify333(payload_ptr != NULL);
+  Verify333(list != NULL);
+
+  // STEP 8: implement LinkedList_Slice.
+
+  return true;  // you may need to change this return value
+}
+
+void LLIterator_Rewind(LLIterator *iter) {
+  iter->node = iter->list->head;
+}
