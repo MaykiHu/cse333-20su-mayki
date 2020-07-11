9da989c891e71b4ac78d1c09cc87f570481cf86a
Travis McGaha <tqmcgaha@attu1.cs.washington.edu>
2020-07-10 20:22:04 -0700

    Files added by CSE 333 staff

---
+++ hw2/README.md
@@ -0,0 +1,25 @@
+# HW2 Short Answer Questions  
+With each HW Assignment, there will be a few short answer questions for you to fill out.  
+These questions will hopefully require you to reflect back on your experiences with the assignment.  
+Please write at least a few sentences for each question.
+
+## Question 1
+##### In this assignment, you had to make substantial use of the data structures in C that you have built so far. As a user of these data-structures, what difficulties did you encounter? If you did not have many difficulties, describe anything notable you encountered while using them. Write >= 3 detailed sentences.
+
+
+
+## Question 2
+##### Creating the file system crawler, indexer, and search engine required working with C-strings (char*). What were a few (>=3) things you had to consider when working with char* compared to the use of a more properly defined String object that would be provided in other languages?
+
+
+
+## Question 3
+##### In this homework, we made use of the POSIX API to read files, and crawl directories. List a couple (>= 2) plausible reasons as to why we used POSIX instead of the C standard library functions for this assignment. Be sure to explain your reasoning.
+
+
+
+## Question 4
+##### In searchshell.c you had to come up with ways of dealing with user input. What were some things (>= 2) you encountered when handling user input that were challenging? How does user input handling compare to other languages you have programmed in? (>=2 comparisons)
+
+
+
---
+++ hw2/searchshell.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright ©2020 Travis McGaha.  All rights reserved.  Permission is
+ * hereby granted to students registered for University of Washington
+ * CSE 333 for use solely during Summer Quarter 2020 for purposes of
+ * the course.  No other use, copying, distribution, or modification
+ * is permitted without prior written consent. Copyrights for
+ * third-party components of this work must be honored.  Instructors
+ * interested in reusing these course materials should contact the
+ * author.
+ */
+
+// Feature test macro for strtok_r (c.f., Linux Programming Interface p. 63)
+#define _XOPEN_SOURCE 600
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#include "libhw1/CSE333.h"
+#include "./CrawlFileTree.h"
+#include "./DocTable.h"
+#include "./MemIndex.h"
+
+//////////////////////////////////////////////////////////////////////////////
+// Helper function declarations, constants, etc
+static void Usage(void);
+static void ProcessQueries(DocTable *dt, MemIndex *mi);
+static int GetNextLine(FILE *f, char **retstr);
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Main
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    Usage();
+  }
+
+  // Implement searchshell!  We're giving you very few hints
+  // on how to do it, so you'll need to figure out an appropriate
+  // decomposition into functions as well as implementing the
+  // functions.  There are several major tasks you need to build:
+  //
+  //  - Crawl from a directory provided by argv[1] to produce and index
+  //  - Prompt the user for a query and read the query from stdin, in a loop
+  //  - Split a query into words (check out strtok_r)
+  //  - Process a query against the index and print out the results
+  //
+  // When searchshell detects end-of-file on stdin (cntrl-D from the
+  // keyboard), searchshell should free all dynamically allocated
+  // memory and any other allocated resources and then exit.
+  return EXIT_SUCCESS;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Helper function definitions
+
+static void Usage(void) {
+  fprintf(stderr, "Usage: ./searchshell <docroot>\n");
+  fprintf(stderr,
+          "where <docroot> is an absolute or relative " \
+          "path to a directory to build an index under.\n");
+  exit(EXIT_FAILURE);
+}
+
+static void ProcessQueries(DocTable *dt, MemIndex *mi) {
+}
+
+static int GetNextLine(FILE *f, char **retstr) {
+  return -1;  // you may want to change this
+}
