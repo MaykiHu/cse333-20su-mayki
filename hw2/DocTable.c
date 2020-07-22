6f716eec4930f50652cdf886074449ca2167b068
Mayki Hu <mayki@uw.edu>
2020-07-21 19:28:14 -0700

    Finished Part B Step 2, DocTable.c, passes valgrind and ./test_suite for this part.

--- hw2/DocTable.c
+++ hw2/DocTable.c
@@ -27,6 +27,9 @@ struct doctable_st {
   DocID_t    max_id;            // max docID allocated so far
 };
 
+// A function for freeing a DocTable's payload
+static void DocValue_Free(HTValue_t value);
+
 DocTable* DocTable_Allocate(void) {
   DocTable *dt = (DocTable *) malloc(sizeof(DocTable));
 
@@ -41,9 +44,9 @@ void DocTable_Free(DocTable *table) {
   Verify333(table != NULL);
 
   // STEP 1.
-
-
-
+  HashTable_Free(table->docid_to_docname, &DocValue_Free);
+  HashTable_Free(table->docname_to_docid, &DocValue_Free);
+  table->max_id = 0;  // empty
   free(table);
 }
 
@@ -63,25 +66,30 @@ DocID_t DocTable_Add(DocTable *table, char *docname) {
   // STEP 2.
   // Check to see if the document already exists.  Then make a copy of the
   // docname and allocate space for the new ID.
-
-
-
+  res = DocTable_GetDocID(table, docname); 
+  if (res != INVALID_DOCID) {  // doc already exists
+    return res;
+  }
+  int nameLength = strlen(docname) + 1;  // for '\0'
+  doccopy = (char *) malloc(nameLength);
+  snprintf(doccopy, nameLength, "%s", docname);  // make a copy of docname
+  docid = (DocID_t *) malloc(sizeof(DocID_t));  // allocate space for new ID
   *docid = table->max_id;
   table->max_id++;
 
   // STEP 3.
   // Set up the key/value for the id->name mapping, and do the insert.
-
-
-
+  kv.key = (HTKey_t) *docid;
+  kv.value = (HTValue_t) doccopy;
+  Verify333(HashTable_Insert(table->docid_to_docname, kv, &oldkv) == false);
   // STEP 4.
   // Set up the key/value for the name->id, and/ do the insert.
   // Be careful about how you calculate the key for this mapping.
   // You want to be sure that how you do this is consistent with
   // the provided code.
-
-
-
+  kv.key = FNVHash64((unsigned char *) doccopy, strlen(doccopy));
+  kv.value = (HTValue_t) docid;
+  Verify333(HashTable_Insert(table->docname_to_docid, kv, &oldkv) == false);
   return *docid;
 }
 
@@ -95,9 +103,11 @@ DocID_t DocTable_GetDocID(DocTable *table, char *docname) {
 
   // STEP 5.
   // Try to find the passed-in doc in docname_to_docid table.
-
-
-
+  key = FNVHash64((unsigned char *) docname, strlen(docname));
+  res = HashTable_Find(table->docname_to_docid, key, &kv);
+  if (res) {  // docname key was found
+    return *(DocID_t *) kv.value;  // return found docid to this docname
+  }
   return INVALID_DOCID;  // you may want to change this
 }
 
@@ -112,9 +122,10 @@ char *DocTable_GetDocName(DocTable *table, DocID_t docid) {
   // and either return the string (i.e., the (char *)
   // saved in the value field for that key) or
   // NULL if the key isn't in the table.
-
-
-
+  bool hasFound = HashTable_Find(table->docid_to_docname, docid, &kv);
+  if (hasFound) {  // docid key was found
+    return kv.value;
+  }
   return NULL;  // you may want to change this
 }
 
@@ -126,4 +137,9 @@ HashTable* DT_GetDocidToDocnameTable(DocTable *table) {
 HashTable* DT_GetDocnameToDocidTable(DocTable *table) {
   Verify333(table != NULL);
   return table->docname_to_docid;
+}
+
+static void DocValue_Free(HTValue_t value) {
+  Verify333(value != NULL);
+  free(value);
 }
